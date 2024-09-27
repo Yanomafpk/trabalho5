dd1d68d97fe25e0fba3cd5e2cf803cc87de6ca37
Yanomafpk <158591318+Yanomafpk@users.noreply.github.com>
2024-09-27 08:33:42 -0300

    fkokokopkvw

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -4,13 +4,34 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
-
-
+    df = pd.read_csv('epa-sea-level.csv')
+    
     # Create scatter plot
+    x = df['Year']
+    y = df['CSIRO Adjusted Sea Level']
 
+    plt.scatter(x, y)
+    plt.xlabel('Year')
+    plt.ylabel('CSIRO Adjusted Sea Level')
+    plt.title('Sea Level Changes Over Time')
+    plt.savefig('bar_plot.png')
+    return plt
 
     # Create first line of best fit
+    
+    res = stats.linregress(x, y)
 
+    plt.scatter(x, y, label='Original Data')
+    plt.plot(x, res.intercept + res.slope * x, 'r', label=f'Fitted Line (slope={res.slope:.2f})')
+    plt.xlabel('Year')
+    plt.ylabel('CSIRO Adjusted Sea Level')
+    plt.title('Sea Level Changes Over Time')
+
+    # Add legend
+    plt.legend()
+
+    # Show plot
+    plt.show()
 
     # Create second line of best fit
 
